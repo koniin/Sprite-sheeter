73ae010762dc0fc109f46aa33f53457aed63e1bf
Henrik Aronsson <arnssn@gmail.com>
2015-06-14 18:58:12 +0200

    Refactoring

--- SpriteSheetPacker/ImageCombinator.cs
+++ SpriteSheetPacker/ImageCombinator.cs
@@ -4,7 +4,18 @@ using System.Drawing;
 
 namespace SpriteSheetPacker {
     class ImageCombinator {
+        private delegate void UpdateImageDimensions(Bitmap image, ref int width, ref int height);
+        private delegate int DrawOffset(Graphics g, Bitmap image, int offset);
+
         public static Bitmap CombineVertical(Bitmap[] files){
+            return CombineImages(files, UpdateVerticalImageDimensions, DrawVertical);
+        }
+
+        public static Bitmap CombineHorizontal(Bitmap[] files) {
+            return CombineImages(files, UpdateHorizontalDimensions, DrawHorizontal);
+        }
+
+        private static Bitmap CombineImages(Bitmap[] files, UpdateImageDimensions funcUpdateImageDimensions, DrawOffset drawOffset) {
             Bitmap finalImage = null;
             try{
                 var width = 0;
@@ -12,75 +23,36 @@ namespace SpriteSheetPacker {
 
                 foreach (var image in files){
                     //update the size of the final bitmap
-                    width = image.Width > width ? image.Width : width;
-                    height += image.Height;
+                    funcUpdateImageDimensions(image, ref width, ref height);
                 }
                 finalImage = new Bitmap(width, height);
 
-                using (Graphics g = Graphics.FromImage(finalImage)) {
+                using (Graphics g = Graphics.FromImage(finalImage)){
                     //set background color
                     g.Clear(Color.Transparent);
 
                     //go through each image and draw it on the final image
                     int offset = 0;
-                    foreach (Bitmap image in files) {
-                        g.DrawImage(image, new Rectangle(0, offset, image.Width, image.Height));
-                        offset += image.Height;
+                    foreach (Bitmap image in files){
+                        offset = drawOffset(g, image, offset);
                     }
                 }
-
-            } catch (Exception) {
+            }
+            catch (Exception){
                 if (finalImage != null)
                     finalImage.Dispose();
                 throw;
-            } finally {
+            }
+            finally{
                 //clean up memory
-                foreach (var image in files) {
+                foreach (var image in files){
                     image.Dispose();
                 }
             }
             return finalImage;
         }
 
-        public static Bitmap CombineHorizontal(Bitmap[] files) {
-            Bitmap finalImage = null;
-            try {
-                var width = 0;
-                var height = 0;
-
-                foreach (var image in files) {
-                    //update the size of the final bitmap
-                    width += image.Width;
-                    height = image.Height > height ? image.Height : height;
-                }
-                finalImage = new Bitmap(width, height);
-
-                using (Graphics g = Graphics.FromImage(finalImage)) {
-                    //set background color
-                    g.Clear(Color.Transparent);
-
-                    //go through each image and draw it on the final image
-                    int offset = 0;
-                    foreach (Bitmap image in files) {
-                        g.DrawImage(image, new Rectangle(offset, 0, image.Width, image.Height));
-                        offset += image.Width;
-                    }
-                }
-
-            } catch (Exception) {
-                if (finalImage != null)
-                    finalImage.Dispose();
-                throw;
-            } finally {
-                //clean up memory
-                foreach (var image in files) {
-                    image.Dispose();
-                }
-            }
-            return finalImage;
-        }
-
-        public static Bitmap Combine(string[] files) {
+        public static Bitmap CombineHorizontal(string[] files) {
             //read all images into memory
             var images = new List<Bitmap>();
             Bitmap finalImage = null;
@@ -111,9 +83,8 @@ namespace SpriteSheetPacker {
                     //go through each image and draw it on the final image
                     var offset = 0;
                     foreach (var image in images) {
-                        g.DrawImage(image,
-                          new Rectangle(offset, 0, image.Width, image.Height));
-                        offset += image.Height;
+                        g.DrawImage(image, new Rectangle(offset, 0, image.Width, image.Height));
+                        offset += image.Width;
                     }
                 }
 
@@ -129,6 +100,28 @@ namespace SpriteSheetPacker {
                     image.Dispose();
                 }
             }
+        }
+
+        private static void UpdateHorizontalDimensions(Bitmap image, ref int width, ref int height) {
+            width += image.Width;
+            height = image.Height > height ? image.Height : height;
+        }
+
+        private static void UpdateVerticalImageDimensions(Bitmap image, ref int width, ref int height) {
+            width = image.Width > width ? image.Width : width;
+            height += image.Height;
+        }
+
+        private static int DrawVertical(Graphics g, Bitmap image, int offset) {
+            g.DrawImage(image, new Rectangle(0, offset, image.Width, image.Height));
+            offset += image.Height;
+            return offset;
+        }
+
+        private static int DrawHorizontal(Graphics g, Bitmap image, int offset) {
+            g.DrawImage(image, new Rectangle(offset, 0, image.Width, image.Height));
+            offset += image.Width;
+            return offset;
         }
     }
 }
--- SpriteSheetPacker/Program.cs
+++ SpriteSheetPacker/Program.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Drawing;
+﻿using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
@@ -7,14 +6,12 @@ using System.Linq;
 namespace SpriteSheetPacker {
     class Program {
         static void Main(string[] args){
-            CombineAll(@"C:\temp\Sprites\player");
-
-            //CreateSpriteSheet(@"C:\temp\Sprites\Player32_full");
-
-            //SplitSheet(@"C:\temp\Sprites\player32_full.png");
+            //CombineAllFromSubFolders(@"C:\temp\Sprites\player");
+            //CombineAllInFolder(@"C:\temp\Sprites\Platformer", @"C:\temp\Sprites\");
+            //SplitSheet(@"C:\temp\Sprites\items\items32.png");
         }
 
-        private static void CombineAll(string folder){
+        private static void CombineAllFromSubFolders(string folder) {
             var sprites = Directory.GetDirectories(folder).Select(GetSpriteSheet).ToList();
             Bitmap sheet = ImageCombinator.CombineHorizontal(sprites.ToArray());
             var fileName = new DirectoryInfo(folder).Name + ".png";
@@ -24,7 +21,7 @@ namespace SpriteSheetPacker {
         private static Bitmap GetSpriteSheet(string folder){
             var dir = new DirectoryInfo(folder);
             var files = Directory.GetFiles(folder);
-            return ImageCombinator.Combine(files);
+            return ImageCombinator.CombineHorizontal(files);
         }
 
         private static void SplitSheet(string inFile){
@@ -38,14 +35,21 @@ namespace SpriteSheetPacker {
 
             Directory.CreateDirectory(newPath);
 
-            for (int i = 0; i < sprites.Count(); i++)
-                sprites[i].Save(Path.Combine(newPath, i + fileInfo.Extension));
+            for (int i = 0; i < sprites.Count(); i++){
+                string filename = GetFileName(i, fileInfo.Extension);
+                sprites[i].Save(Path.Combine(newPath, filename));
+            }
         }
 
-        private static void CreateSpriteSheet(string folder, string outfolder){
+        private static string GetFileName(int i, string extension){
+            return (i + extension).PadLeft(7, '0');
+        }
+
+        private static void CombineAllInFolder(string folder, string outfolder) {
             var dir = new DirectoryInfo(folder);
-            var files = Directory.GetFiles(folder);
-            var image = ImageCombinator.Combine(files);
+            var files = Directory.GetFiles(folder).ToList();
+            files.Sort();
+            var image = ImageCombinator.CombineHorizontal(files.ToArray());
             image.Save(Path.Combine(outfolder, dir.Name + ".png"), ImageFormat.Png);
         }
     }
